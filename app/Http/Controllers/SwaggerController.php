6480dec440e64b0ec5d2c3dbe614c0f59c1e7189
rivesuvacrispa <vova.cpeda@yandex.ru>
2026-04-21 12:33:00 +0300

    Feature (Swagger) - добавление базовых спецификаций api и boilerplate спецификации товара

--- app/Http/Controllers/SwaggerController.php
+++ app/Http/Controllers/SwaggerController.php
@@ -25,6 +25,43 @@ use OpenApi\Annotations as OA;
  *     description="local dev server"
  * )
  *
+ * @OA\Schema(
+ *       schema="ValidationError",
+ *       @OA\Property(property="message", type="string", example="The *** field must be a ***. (and 1 more error)"),
+ *       @OA\Property(property="errors", type="object", example={"field1": {"The *** field must be a ***."}, "field2": {"The *** field must be *** characters."}})
+ *   )
+ *
+ * @OA\Response(
+ *      response="ValidationResponse",
+ *      description="Ошибка валидации данных",
+ *      @OA\JsonContent(ref="#/components/schemas/ValidationError")
+ *  )
+ *
+ * @OA\Schema(
+ *        schema="NotFoundError",
+ *        @OA\Property(property="message", type="string", example="The requested resource is not found or unavailable."),
+ *    )
+ *
+ * @OA\Response(
+ *       response="NotFoundError",
+ *       description="Сущность не найдена",
+ *       @OA\JsonContent(ref="#/components/schemas/NotFoundError")
+ *   )
+ *
+ * @OA\Schema(
+ *      schema="PaginationMeta",
+ *      title="Пагинация",
+ *      @OA\Property(property="page", type="integer", example=1),
+ *      @OA\Property(property="per_page", type="integer", example=10),
+ *      @OA\Property(property="total", type="integer", example=50),
+ *      @OA\Property(property="total_pages", type="integer", example=5),
+ *  )
+ *
+ * @OA\Schema(
+ *     schema="Product",
+ *     title="Товар",
+ *     @OA\Property(property="name", type="string", example="Товар 1"),
+ * )
  *
  */
 class SwaggerController extends Controller
