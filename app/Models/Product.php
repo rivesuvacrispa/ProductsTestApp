e85234425b3836dad14caea36dd6ce0a05f0c6ef
rivesuvacrispa <vova.cpeda@yandex.ru>
2026-04-21 11:52:33 +0300

    Feature (Products) - Json ресурс для сущности товаров

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Http\Resources\ProductResource;
+use Illuminate\Database\Eloquent\Attributes\UseResource;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+#[UseResource(ProductResource::class)]
 class Product extends Model
 {
     use HasFactory;
