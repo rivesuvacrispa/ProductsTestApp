f28f4b97dc6e7c6f132b5df95897396a400d6905
rivesuvacrispa <vova.cpeda@yandex.ru>
2026-04-21 11:49:25 +0300

    Feature (Database) - Миграция и модель для сущности товаров

---
+++ app/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'in_stock',
+        'rating',
+        'category_id',
+    ];
+
+    protected $casts = [
+          'in_stock' => 'boolean'
+    ];
+
+    public function category() : BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
---
+++ database/migrations/2026_04_21_083754_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name')->fulltext();
+            $table->decimal('price')->default(0.00);
+            $table->boolean('in_stock')->default(1);
+            $table->decimal('rating')->default(0);
+
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->foreign('category_id')->references('id')->on('categories')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
