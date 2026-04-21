b28796cb72ad4bc829fe7a428de8e42940d6bfb7
rivesuvacrispa <vova.cpeda@yandex.ru>
2026-04-21 11:49:41 +0300

    Feature (Database) - Фабрика и сидер для сущности товаров

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->words(2, true),
+            'in_stock' => $this->faker->boolean(75),
+            'rating' => $this->faker->randomFloat(2,0, 5),
+        ];
+    }
+}
