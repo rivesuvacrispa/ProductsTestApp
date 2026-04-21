00db1996cd30382a4d05dc788fc28884c92f3c47
rivesuvacrispa <vova.cpeda@yandex.ru>
2026-04-21 14:03:53 +0300

    Feature (ProductFactory) - Фабрика товара использует случайную существующую категорию для category_id

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -16,6 +17,7 @@ class ProductFactory extends Factory
             'in_stock' => $this->faker->boolean(75),
             'rating' => $this->faker->randomFloat(2,0, 5),
             'price' => $this->faker->randomFloat(2,10, 100),
+            'category_id' => Category::inRandomOrder()->first()->id,
         ];
     }
 }
